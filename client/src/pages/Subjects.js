f8817a4b4c2b8653553d0a00158c94ef59d9b173
PsymonSezArcedera <pearcedera@up.edu.ph>
2024-05-08 11:20:16 +0800

    chore: greetings

--- client/src/pages/Subjects.js
+++ client/src/pages/Subjects.js
@@ -1,11 +1,34 @@
-const subjects = [{ code: 'CMSC 100' }, { code: 'CMSC 23'}]
+import { useEffect, useState } from 'react';
 
 export default function Subjects() {
 
+  const [ subjects, setSubjects ] = useState([])
+  const [ greeting, setGreeting ] = useState('')
+  useEffect(() => {
+    fetch('http://localhost:3001/get-subjects')
+      .then(response => response.json())
+      .then(body => {
+        setSubjects(body)
+      })
+
+      fetch('http://localhost:3001/greet-by-post',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name: 'Monina'})
+      })
+      .then(response => response.text())
+      .then(body => {
+        setGreeting(body)
+      })
+  }, [])
   return (
     <>
       <ol>
         { subjects.map((subject, i) => <li key={i}>{subject.code}</li>) }
+        <div>{ greeting }</div>      
       </ol>
     </>
   )
