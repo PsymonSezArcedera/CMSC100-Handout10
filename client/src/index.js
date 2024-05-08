1d5802ebdf2681c153bc38418dd27e5a8f0c4800
PsymonSezArcedera <pearcedera@up.edu.ph>
2024-05-08 11:15:31 +0800

    chore: restructure

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css'
+
+import Root from './pages/Root';
+import Subjects from './pages/Subjects';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Root />, children: [
+    { path: '/', element: <Home /> },
+    { path: 'subjects', element: <Subjects /> }
+  ]}
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />    
+  </React.StrictMode>
+);
